180dacff34e09bfa314d0514cddf4115731f0c20
Luiz <luizpfern14@gmail.com>
2024-04-19 19:48:52 -0300

    Implementando banco

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,13 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { createPool } from '@vercel/postgres';
+
+const pool = createPool({
+  connectionString: '',
+});
+
+
+// A one-shot query
 
 @Component({
   selector: 'app-home',
@@ -13,11 +21,17 @@ export class HomePage {
   login = {user:'', pass:''}
 
 
-  submitLogin(user:string, pass:string) {
+  async submitLogin(user:string, pass:string) {
     console.log(user)
     console.log(pass)
-
-    // Comando sql que busca usuario
+    const query = `SELECT * FROM USUARIOS WHERE login='${user}' and senha='${pass}'`
+    const response = await pool.sql`SELECT * FROM USUARIOS`
+    console.log(response.rows)
+    if (response.rows.length == 0) {
+      console.log('Usuario ou senha invalido')
+    } else {
+      console.log('Login valido, ir para outra tela')
+    }
   }
 
 }
