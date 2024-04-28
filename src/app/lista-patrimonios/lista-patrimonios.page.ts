2063dbcdb814e5d885f0ce2f2346324be35a4a03
Luiz <luizpfern14@gmail.com>
2024-04-28 19:20:21 -0300

    Botao de voltar em lista de patrimonios

--- src/app/lista-patrimonios/lista-patrimonios.page.ts
+++ src/app/lista-patrimonios/lista-patrimonios.page.ts
@@ -22,6 +22,10 @@ export class ListaPatrimoniosPage implements OnInit {
     this.router.navigateByUrl('/patrimonios/'+id);
   }
 
+  navigateBack() {
+    this.router.navigateByUrl('/principal')
+  }
+
   async filtro() {
     let data = [{ "id": 1, "first_name": "Jean", "last_name": "Owens", "email": "jowens0@google.ru", "gender": "Female" }, { "id": 2, "first_name": "Marie", "last_name": "Morris", "email": "mmorris1@engadget.com", "gender": "Female" }, { "id": 3, "first_name": "Larry", "last_name": "Wallace", "email": "lwallace2@example.com", "gender": "Male" }],
     keys = Object.keys(data[0]),
