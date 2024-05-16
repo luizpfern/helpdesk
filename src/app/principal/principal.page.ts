2c26624ebcc652211705a0ff3cbeaac99b872b69
Luiz <luizpfern14@gmail.com>
2024-05-16 10:41:15 -0300

    Implementacao de graficos

--- src/app/principal/principal.page.ts
+++ src/app/principal/principal.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ServidorService } from '../servidor.service';
 import { Router } from '@angular/router';
+import { Chart } from 'chart.js';
 
 @Component({
   selector: 'app-principal',
@@ -12,6 +13,17 @@ export class PrincipalPage implements OnInit {
   constructor(public servidor:ServidorService, private router:Router) { }
 
   ngOnInit() {
+
+    const data = [
+      { year: 2010, count: 10 },
+      { year: 2011, count: 20 },
+      { year: 2012, count: 15 },
+      { year: 2013, count: 25 },
+      { year: 2014, count: 22 },
+      { year: 2015, count: 30 },
+      { year: 2016, count: 28 },
+    ];
+  
   }
 
   open(path:string) {
